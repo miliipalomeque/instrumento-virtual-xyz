b068d59c949cbf8c8d39f9ddf71bfa1fb089f433
Milagros Palomeque <miliipalomeque@gmail.com>
2024-11-21 12:34:23 -0300

    Añadir estructura inicial del proyecto

---
+++ script.js
@@ -0,0 +1,6 @@
+// Función para mostrar la demostración cuando se hace clic en el botón
+function mostrarDemo() {
+    // Muestra una alerta con un mensaje de demostración
+    alert("¡Demostración iniciada! Imagina que este es tu instrumento virtual en acción.");
+  }
+  
